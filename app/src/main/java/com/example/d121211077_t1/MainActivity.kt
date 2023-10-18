0d2fd1347db83cdccd346b77c929aea6c8662066
Rodey18 <raihky.main@gmail.com>
2023-10-19 03:29:20 +0800

    Update UI

--- app/src/main/java/com/example/d121211077_t1/MainActivity.kt
+++ app/src/main/java/com/example/d121211077_t1/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -26,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,94 +49,89 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+fun Contact(teks: String, icon: ImageVector) {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Row(
+            modifier = Modifier.padding(vertical = 2.dp, horizontal = 45.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = Color(android.graphics.Color.parseColor("#008789")),
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(horizontal = 8.dp)
+                    .size(16.dp)
+            )
+            Text(
+                text = teks,
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Left
+            )
+        }
+    }
+    }
 
 @Composable
 fun BusinessCard() {
     Column(
-        modifier = Modifier.height(450.dp).width(300.dp).background(Color(android.graphics.Color.parseColor("#D3FADE"))),
+        modifier = Modifier
+            .height(600.dp)
+            .width(300.dp)
+            .background(Color(android.graphics.Color.parseColor("#D3FADE"))),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally) {
         Column(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
                 painter = painterResource(id = R.drawable.android_logo),
-                contentDescription = "Logo",
-                modifier = Modifier.size(100.dp).background(Color(android.graphics.Color.parseColor("#2F4858"))),
+                contentDescription = "Logo-Android",
+                modifier = Modifier
+                    .size(75.dp)
+                    .background(Color(android.graphics.Color.parseColor("#00525A"))),
 
             )
             Text(
                 text = "Raihky Ra'ba Bara' Tiku",
                 style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.fillMaxWidth().padding(top = 16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
                 textAlign = TextAlign.Center
             )
             Text(
-                text = "Android Developer",
+                text = "Android Developer Jr.",
                 style = MaterialTheme.typography.titleSmall,
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center,
-                color = Color(android.graphics.Color.parseColor("#00A29B"))
+                color = Color(android.graphics.Color.parseColor("#008789"))
             )
         }
         Column(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().padding(bottom = 45.dp),
             verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.Start
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Icon(
-                    Icons.Default.Phone,
-                    contentDescription = null,
-                    modifier = Modifier.align(Alignment.CenterVertically).padding(horizontal = 8.dp).size(16.dp)
-                )
-                Text(
-                    text = "+62 8529 8695 867",
-                    style = MaterialTheme.typography.bodySmall,
-                    textAlign = TextAlign.Left
-                )
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Icon(
-                    Icons.Default.Share,
-                    contentDescription = null,
-                    modifier = Modifier.align(Alignment.CenterVertically).padding(horizontal = 8.dp).size(16.dp)
-                )
-                Text(
-                    text = "@raihky_t",
-                    style = MaterialTheme.typography.bodySmall,
-                    textAlign = TextAlign.Left
-                )
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Icon(
-                    Icons.Default.Email,
-                    contentDescription = null,
-                    modifier = Modifier.align(Alignment.CenterVertically).padding(horizontal = 8.dp).size(16.dp)
-                )
-                Text(
-                    text = "raihky.education@gmail.com",
-                    style = MaterialTheme.typography.bodySmall,
-                    textAlign = TextAlign.Left
-                )
+            Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally){
+                Contact(teks = "+62 8529 8695 867", icon = Icons.Default.Phone)
+                Contact(teks = "@raihky_t", icon = Icons.Default.Share)
+                Contact(teks = "raihky.education@gmail.com", icon = Icons.Default.Email)
             }
         }
     }
 }
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TugasPreview() {
     D121211077_T1Theme {
         BusinessCard()
     }
